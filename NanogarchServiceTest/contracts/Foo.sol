367afc163626578cafe40f67ff17202187f11bd7
Aaron St. John <aaronbstjohn@gmail.com>
2018-09-20 13:11:57 -0400

    Stuff

---
+++ NanogarchServiceTest/contracts/Foo.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.2;
+contract Foo {
+  function bar(fixed[2] xy) {}
+  function baz(uint32 x, bool y) returns (bool r) { r = x > 32 || y; }
+  function sam(bytes name, bool z, uint[] data) {}
+}
